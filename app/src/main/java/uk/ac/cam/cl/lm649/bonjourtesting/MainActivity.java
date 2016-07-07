ef5ef9c2324ee507fe18a32ade2e471e4b50fb28
bloodx <makklaci@gmail.com>
2016-07-07 17:56:31 +0100

    resetting port number on the UI onStop

--- app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/MainActivity.java
+++ app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends Activity {
     private void resetUI(){
         Log.i(TAG, "Resetting UI.");
         textViewDeviceIp.setText("");
+        textViewLocalPort.setText("");
         textViewOwnService.setText("");
         listAdapter.clear();
     }
