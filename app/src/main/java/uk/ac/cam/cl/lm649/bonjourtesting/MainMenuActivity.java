efe30583a6cf1d826a51abfc41b03c7ed74bf773
bloodx <makklaci@gmail.com>
2016-07-20 20:07:06 +0100

    MainMenu is now also a CustomActivity

--- app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/MainMenuActivity.java
+++ app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/MainMenuActivity.java
@@ -1,12 +1,11 @@
 package uk.ac.cam.cl.lm649.bonjourtesting;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainMenuActivity extends Activity {
+public class MainMenuActivity extends CustomActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
