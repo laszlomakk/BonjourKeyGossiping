806580505f06b797e66e6b56c00ddb06c6c4841a
bloodx <makklaci@gmail.com>
2016-07-26 11:23:18 +0100

    added functionality: user can delete badges from DB

--- app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/activebadge/ActiveBadgeActivity.java
+++ app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/activebadge/ActiveBadgeActivity.java
@@ -1,8 +1,11 @@
 package uk.ac.cam.cl.lm649.bonjourtesting.activebadge;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -12,6 +15,7 @@ import java.util.ArrayList;
 import uk.ac.cam.cl.lm649.bonjourtesting.CustomActivity;
 import uk.ac.cam.cl.lm649.bonjourtesting.R;
 import uk.ac.cam.cl.lm649.bonjourtesting.util.FLogger;
+import uk.ac.cam.cl.lm649.bonjourtesting.util.HelperMethods;
 import uk.ac.cam.cl.lm649.bonjourtesting.util.NetworkUtil;
 
 public class ActiveBadgeActivity extends CustomActivity {
@@ -19,6 +23,8 @@ public class ActiveBadgeActivity extends CustomActivity {
     private static final String TAG = "ActiveBadgeActivity";
 
     private ArrayAdapter<String> listAdapterForDisplayedListOfBadges;
+    private ArrayList<Badge> badgesArrList = new ArrayList<>();
+    private final Object displayedBadgesLock = new Object();
 
     private TextView textViewBadgeId;
     private TextView textViewCustomName;
@@ -39,6 +45,36 @@ public class ActiveBadgeActivity extends CustomActivity {
         ListView listView = (ListView) findViewById(R.id.mainListView);
         listAdapterForDisplayedListOfBadges = new ArrayAdapter<>(this, R.layout.active_badge_row_in_list, new ArrayList<String>());
         listView.setAdapter(listAdapterForDisplayedListOfBadges);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                synchronized (displayedBadgesLock) {
+                    FLogger.i(TAG, "onItemClick() - user clicked on an item in the list");
+                    final Badge badge = badgesArrList.get(position);
+                    if (null == badge){
+                        FLogger.e(TAG, "onItemClick(). clicked badge is null ??");
+                        HelperMethods.displayMsgToUser(context, "error: clicked badge is null");
+                        return true;
+                    }
+                    DialogInterface.OnClickListener deleteBadge = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            FLogger.i(TAG, "user deleted badge from DB");
+                            FLogger.d(TAG, "deleted badge had details: " + badge.toString());
+                            BadgeDbHelper.getInstance(app).deleteBadge(badge);
+                            updateListView();
+                        }
+                    };
+                    new AlertDialog.Builder(ActiveBadgeActivity.this)
+                            .setTitle("Confirm Delete")
+                            .setMessage("Do you really want to delete that badge?")
+                            //.setIcon(R.drawable.maybe_a_cool_icon_here)
+                            .setPositiveButton(android.R.string.yes, deleteBadge)
+                            .setNegativeButton(android.R.string.no, null).show();
+                    return true;
+                }
+            }
+        });
 
         // top area
         textViewBadgeId = (TextView) findViewById(R.id.badgeId);
@@ -74,7 +110,10 @@ public class ActiveBadgeActivity extends CustomActivity {
 
         textViewRouterMac.setText(NetworkUtil.getRouterMacAddress(context));
 
-        String numBadgesInList = "" + listAdapterForDisplayedListOfBadges.getCount();
+        String numBadgesInList;
+        synchronized (displayedBadgesLock) {
+            numBadgesInList = "" + listAdapterForDisplayedListOfBadges.getCount();
+        }
         textViewNumBadgesInList.setText(numBadgesInList);
     }
 
@@ -83,13 +122,17 @@ public class ActiveBadgeActivity extends CustomActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                FLogger.v(TAG, "updateListView() doing actual update.");
-                listAdapterForDisplayedListOfBadges.clear();
-                for (Badge badge : BadgeDbHelper.getInstance(context).getAllBadges()) {
-                    listAdapterForDisplayedListOfBadges.add(badge.toString());
+                synchronized (displayedBadgesLock) {
+                    FLogger.v(TAG, "updateListView() doing actual update.");
+                    listAdapterForDisplayedListOfBadges.clear();
+                    badgesArrList.clear();
+                    for (Badge badge : BadgeDbHelper.getInstance(context).getAllBadges()) {
+                        listAdapterForDisplayedListOfBadges.add(badge.toString());
+                        badgesArrList.add(badge);
+                    }
+                    listAdapterForDisplayedListOfBadges.notifyDataSetChanged();
+                    refreshTopUIInternal();
                 }
-                listAdapterForDisplayedListOfBadges.notifyDataSetChanged();
-                refreshTopUIInternal();
             }
         });
     }
