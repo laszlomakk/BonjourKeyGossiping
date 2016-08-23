83cd516c76b379d3a1f7da06ff6e6561ff57dad9
bloodx <makklaci@gmail.com>
2016-08-23 17:28:53 +0100

    minor clean-up in PublicKeysActivity regarding storing UI elements

--- app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/menu/PublicKeysActivity.java
+++ app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/menu/PublicKeysActivity.java
@@ -24,8 +24,7 @@ public class PublicKeysActivity extends CustomActivity {
 
     private static final String TAG = "PublicKeysActivity";
 
-    private ArrayAdapter<String> listAdapterForDisplayedListOfEntries;
-    private ArrayList<DbTablePublicKeys.EntryWithName> entriesArrList = new ArrayList<>();
+    private ArrayAdapter<DbTablePublicKeys.EntryWithName> listAdapterForDisplayedListOfEntries;
     private final Object displayedEntriesLock = new Object();
 
     private TextView textViewCustomName;
@@ -45,14 +44,15 @@ public class PublicKeysActivity extends CustomActivity {
 
         // listView for displaying the entries
         ListView listView = (ListView) findViewById(R.id.mainListView);
-        listAdapterForDisplayedListOfEntries = new ArrayAdapter<>(this, R.layout.row_in_list, new ArrayList<String>());
+        listAdapterForDisplayedListOfEntries = new ArrayAdapter<>(
+                this, R.layout.row_in_list, new ArrayList<DbTablePublicKeys.EntryWithName>());
         listView.setAdapter(listAdapterForDisplayedListOfEntries);
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 synchronized (displayedEntriesLock) {
                     FLogger.i(TAG, "onItemClick() - user clicked on an item in the list");
-                    final DbTablePublicKeys.Entry entry = entriesArrList.get(position);
+                    final DbTablePublicKeys.Entry entry = listAdapterForDisplayedListOfEntries.getItem(position);
                     if (null == entry){
                         FLogger.e(TAG, "onItemClick(). clicked entry is null ??");
                         HelperMethods.displayMsgToUser(context, "error: clicked entry is null");
@@ -130,10 +130,8 @@ public class PublicKeysActivity extends CustomActivity {
                 synchronized (displayedEntriesLock) {
                     FLogger.v(TAG, "updateListView() doing actual update.");
                     listAdapterForDisplayedListOfEntries.clear();
-                    entriesArrList.clear();
                     for (DbTablePublicKeys.EntryWithName entry : DbTablePublicKeys.getAllEntries()) {
-                        listAdapterForDisplayedListOfEntries.add(entry.toString());
-                        entriesArrList.add(entry);
+                        listAdapterForDisplayedListOfEntries.add(entry);
                     }
                     listAdapterForDisplayedListOfEntries.notifyDataSetChanged();
                     refreshTopUIInternal();
