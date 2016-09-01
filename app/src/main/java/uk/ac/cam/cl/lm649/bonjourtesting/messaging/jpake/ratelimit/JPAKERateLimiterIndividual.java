562641826f2ab53ead6e0b721ed2a80e2e3c53f2
bloodx <makklaci@gmail.com>
2016-09-01 12:24:44 +0100

    JPAKE rate-limiting (individual): periodically iterating through the map and garbage collecting old data, to avoid the map getting too large

--- app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/messaging/jpake/ratelimit/JPAKERateLimiterIndividual.java
+++ app/src/main/java/uk/ac/cam/cl/lm649/bonjourtesting/messaging/jpake/ratelimit/JPAKERateLimiterIndividual.java
@@ -6,6 +6,7 @@ import java.net.InetAddress;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 import uk.ac.cam.cl.lm649.bonjourtesting.Constants;
@@ -17,12 +18,18 @@ public class JPAKERateLimiterIndividual {
 
     // key: MAC address
     // value: sorted list of timestamps of failed JPAKE handshakes since the last successful handshake
-    // TODO periodically iterate through map to delete old values, ensuring its size stays reasonable
     protected final ConcurrentSkipListMap<String, List<Long>> individualJpakeHandshakeTimestamps = new ConcurrentSkipListMap<>();
     private static final long INDIVIDUAL_LIMIT_CONSIDERS_TIME_INTERVAL = 30 * Constants.MSECONDS_IN_MINUTE;
     private static final int INDIVIDUAL_LIMIT_ALLOWS_NUM_HANDSHAKES_IN_INTERVAL = 10;
 
-    protected JPAKERateLimiterIndividual() {}
+    // we should periodically iterate through the map to delete old values, ensuring its size stays reasonable
+    // this is the minimum time between two such "garbage collections"
+    private static final long GARBAGE_COLLECTION_TIME_PERIOD = 4 * Constants.MSECONDS_IN_HOUR;
+    private long timeOfLastGarbageCollection;
+
+    protected JPAKERateLimiterIndividual() {
+        timeOfLastGarbageCollection = JPAKERateLimiter.getCurrentTimestamp();
+    }
 
     /**
      * @param ipAddress IP of other participant
@@ -31,6 +38,8 @@ public class JPAKERateLimiterIndividual {
      */
     protected boolean canStartNewJpakeHandshake(@Nullable InetAddress ipAddress, @Nullable String macAddress) {
         String strIpAddress = null == ipAddress ? "null" : ipAddress.getHostAddress();
+
+        considerDoingGarbageCollection();
 
         if (null == macAddress) {
             FLogger.e(TAG, "canStartNewJpakeHandshake(). macAddress == null. (IP: " + strIpAddress + ")");
@@ -65,6 +74,28 @@ public class JPAKERateLimiterIndividual {
                 individualTimestamps.add(curTime);
             }
         }
+    }
+
+    private synchronized void considerDoingGarbageCollection() {
+        long curTime = JPAKERateLimiter.getCurrentTimestamp();
+        if (timeOfLastGarbageCollection < curTime - GARBAGE_COLLECTION_TIME_PERIOD) {
+            doGarbageCollection();
+            timeOfLastGarbageCollection = curTime;
+        }
+    }
+
+    private void doGarbageCollection() {
+        FLogger.i(TAG, "doGarbageCollection() called.");
+
+        int counter = 0;
+        for (Map.Entry<String, List<Long>> entry : individualJpakeHandshakeTimestamps.entrySet()) {
+            JPAKERateLimiter.deleteOldTimestampsFromList(entry.getValue(), INDIVIDUAL_LIMIT_CONSIDERS_TIME_INTERVAL);
+            if (entry.getValue().isEmpty()) {
+                individualJpakeHandshakeTimestamps.remove(entry.getKey());
+                counter++;
+            }
+        }
+        FLogger.d(TAG, "doGarbageCollection() removed " + counter + " lists from map");
     }
 
 }
